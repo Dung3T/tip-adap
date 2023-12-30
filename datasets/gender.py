d62d73e70c6870bcea40efb785547bd4107da3ef
Dung3T <21521983@gm.uit.edu.vn>
2023-12-30 12:13:36 +0700

    Initial commit

--- datasets/gender.py
+++ datasets/gender.py
@@ -14,7 +14,7 @@ class Gender(DatasetBase):
     def __init__(self, root, num_shots):
         self.dataset_dir = os.path.join(root, self.dataset_dir)
         self.image_dir = os.path.join(self.dataset_dir, 'gender2')
-        self.split_path = os.path.join(self.dataset_dir, 'output3.json')
+        self.split_path = os.path.join(self.image_dir, 'output3.json')
 
         self.template = template
 
--- datasets/image.py
+++ datasets/image.py
@@ -14,7 +14,7 @@ class Image(DatasetBase):
     def __init__(self, root, num_shots):
         self.dataset_dir = os.path.join(root, self.dataset_dir)
         self.image_dir = os.path.join(self.dataset_dir, 'dog2')
-        self.split_path = os.path.join(self.dataset_dir, 'output3.json')
+        self.split_path = os.path.join(self.image_dir, 'output3.json')
 
         self.template = template
 
